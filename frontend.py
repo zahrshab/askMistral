7735d56f9c8b9f4763ac41c27a79766fecd7c9b0
zahrshab <shab_al12@hotmail.com>
2025-08-26 22:18:51 +0200

    improved frontend

--- frontend.py
+++ frontend.py
@@ -4,11 +4,13 @@ import json
 import emoji
 import regex
 import re
-import random
 import threading
 from datetime import datetime
 
 st.set_page_config(page_title="Ask Mistral", page_icon="🧠")
+
+port = "8686"
+url = "http://localhost:" + port + "/ask"
 
 if "suggestions" not in st.session_state:
     try:
@@ -23,7 +25,7 @@ def save_json(filepath, data):
         json.dump(data, file, indent=2)
         file.truncate()
 
-def edit_history():
+def show_chat_history():
     with open("prompts.json", "r") as file:
         prompts = json.load(file)
         for item in prompts:
@@ -39,9 +41,6 @@ def edit_history():
                 st.write("Answer:", item["response"])
                 st.write("Timestamp:", item["timestamp"])
 
-def extract_questions(text):
-    return re.findall(r"\d+\.\s(.*?\?)", text)
-
 st.title("🧠 AskMistral – Your local AI")
 
 if "suggestion_prompt" not in st.session_state:
@@ -53,24 +52,13 @@ with st.sidebar:
 
     with col1:
         st.markdown("### Recently asked")
-        edit_history()
+        show_chat_history()
 
     with col2:
         if st.button("Reset"):
             with open("prompts.json", "w") as file:
                 json.dump([], file, indent=2)
             st.rerun()
-
-emoji_detector = ",start your answer with a related emoji"
-
-def extract_first_emoji_and_text(text):
-    graphemes = regex.findall(r'\X', text)
-
-    for i, g in enumerate(graphemes):
-        if emoji.is_emoji(g):
-            return g, ''.join(graphemes[i+1:]).lstrip()
-
-    return None, text
 
 def write_answer(user_input, answer, emoji):
     st.markdown("### 💬 Answer:")
@@ -90,30 +78,26 @@ def write_answer(user_input, answer, emoji):
         file.seek(0)
         json.dump(data, file, indent=2)
 
-def generateQuestions():
-    user_input = "Generate 3 questions"
+def extract_first_emoji_and_text(text):
+    graphemes = regex.findall(r'\X', text)
+
+    for i, g in enumerate(graphemes):
+        if emoji.is_emoji(g):
+            return g, ''.join(graphemes[i+1:]).lstrip()
+
+    return None, text
+
+emoji_detector = ",start your answer with a related emoji"
+
+#user has asked a question
+
+def ask_question(user_input):
+    st.session_state["suggestion_prompt"] = None #resetten 
     try:
         response = requests.post(
-            "http://localhost:8000/ask", 
-            json={"prompt": user_input}
-        )
-        result = response.json()
-        questions = extract_questions(result["response"])
-        return questions
-    except Exception as e:
-        st.error(f"An error occured: {e}")
-
-def pickThreeSuggestions():
-    with open("suggestions.json", "r") as file:
-        data = json.load(file)
-    return data[:3]
-
-def question(user_input):
-    st.session_state["suggestion_prompt"] = None
-    try:
-        response = requests.post(
-            "http://localhost:8000/ask", 
-            json={"prompt": user_input + emoji_detector}
+            url, 
+            json={"prompt": user_input + emoji_detector}, 
+            
         )
         result = response.json()
         emoji, answer = extract_first_emoji_and_text(result["response"])
@@ -122,27 +106,33 @@ def question(user_input):
     except Exception as e:
         st.error(f"An error occured: {e}")
 
-
-with column1: 
+def show_input_area(): 
     user_input = st.text_area("What do you want to ask Mistral?", "")
     if st.button("Ask"):
         if user_input.strip() != "":
             with st.spinner("Mistral is thinking..."):
-                question(user_input)
+                ask_question(user_input)
 
-def substitute_oneQuestion(new_question, identifier):
-    st.session_state["suggestion_index"] = None
+with column1: 
+    show_input_area()   
 
-    if 0 <= identifier < len(st.session_state["suggestions"]):
-        st.session_state["suggestions"][identifier]["prompt"] = new_question
-        save_json("suggestions.json", st.session_state["suggestions"])
+def extract_questions(text):
+    return re.findall(r"\d+\.\s(.*?\?)", text)
 
+def generateQuestions():
+    user_input = "Generate 3 questions"
+    try:
+        response = requests.post(
+            url, 
+            json={"prompt": user_input}
+        )
+        result = response.json()
+        questions = extract_questions(result["response"])
+        return questions
+    except Exception as e:
+        st.error(f"An error occured: {e}")
 
-def substitute_allQuestions(questions):
-    data = [{"prompt": question} for question in questions]
-    save_json("suggestions.json", data)
-
-def suggestions(): 
+def show_suggestions(): 
     left, right = st.columns([2, 1])
     with left:
         st.markdown("Frequently asked questions")
@@ -154,26 +144,39 @@ def suggestions():
             save_json("suggestions.json", st.session_state["suggestions"])
 
     for index, item in enumerate(st.session_state["suggestions"][:3]):
-        if st.button(item["prompt"], key=f"suggest_{item['prompt']}_{index}"):
+        if st.button(item["prompt"], key=f"suggest_{item['prompt']}_{index}"): #if one the suggestion buttons is clicked on
             st.session_state["suggestion_prompt"] = item["prompt"]
             st.session_state["suggestion_index"] = index
         
 with column2: 
-    suggestions()
+    show_suggestions()
 
-if st.session_state["suggestion_prompt"]:
+def substitute_oneQuestion(new_question, identifier):
+    st.session_state["suggestion_index"] = None
+
+    if 0 <= identifier < len(st.session_state["suggestions"]):
+        st.session_state["suggestions"][identifier]["prompt"] = new_question
+        save_json("suggestions.json", st.session_state["suggestions"])
+
+def handle_suggested_question_click():
     with column1:
         with st.spinner("Mistral is thinking..."):
             result_container = {}
 
-            def get_questions():
-                result_container["questions"] = generateQuestions()
+            def store_new_suggestions():
+                result_container["questions"] = generateQuestions() 
 
-            thread = threading.Thread(target=get_questions)
+            #threading -> 1. generating 3 new questions 2. asking the clicked-on suggested prompt 
+            thread = threading.Thread(target=store_new_suggestions) #het genereren van 3 questions heeft nauwelijks tot geen effect op performance en is handiger voor question extraction
             thread.start()
-            question(st.session_state["suggestion_prompt"])
+            ask_question(st.session_state["suggestion_prompt"]) #
             thread.join()
 
             questions = result_container.get("questions", [])
             if questions:
-                substitute_oneQuestion(questions[0], st.session_state["suggestion_index"])
+                substitute_oneQuestion(questions[0], st.session_state["suggestion_index"]) #substitute the clicked-on question
+
+
+#if user has clicked on a suggestion prompt
+if st.session_state["suggestion_prompt"]:
+    handle_suggested_question_click()
